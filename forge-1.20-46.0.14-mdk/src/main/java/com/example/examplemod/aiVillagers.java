81f0b3def2d13a516a98d9fae64df755427b3fe8
KOrtizLedezma <kenethortiz2016@gmail.com>
2025-05-01 02:00:46 -0400

    First push

---
+++ forge-1.20-46.0.14-mdk/src/main/java/com/example/examplemod/aiVillagers.java
@@ -0,0 +1,14 @@
+package com.example.examplemod;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(aiVillagers.MODID)
+public class aiVillagers
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "KB-Mod";
+
+    public aiVillagers() {
+
+    }
+
+}
